76f0b70e9b9f452be2c999f8d8af250973b8ce72
matben001 <benoitm007@gmail.com>
2020-09-21 17:24:06 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -27,7 +27,8 @@ def get_bill(name, data):
 def format_number(number, num_decimal_digits):
     #Séparer les parties entières et décimal
     #Formater la parties décimal
-    #Formater la partie ent
+    #Formater la partie entières
+    #Con
 	return ""
 
 def get_triangle(num_rows):
