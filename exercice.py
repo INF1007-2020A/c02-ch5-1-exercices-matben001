f9ee13e480e04d2cb3929a6f3bc30e6181f421f9
matben001 <benoitm007@gmail.com>
2020-09-21 17:07:46 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -18,7 +18,7 @@ def get_bill(name, data):
     
     #rotouer la facture fromatée
     result = name
-    result += "\n"+f"SOUS-total {sum : }"
+    result += "\n"+f"SOUS-total {sum : >10}"
 	return 
 
 
