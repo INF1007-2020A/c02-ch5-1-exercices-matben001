520e5d6add07f8737eff5e7f0655c165e7138500
matben001 <benoitm007@gmail.com>
2020-09-21 17:23:21 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -25,7 +25,7 @@ def get_bill(name, data):
 
 
 def format_number(number, num_decimal_digits):
-    #Séârer les parties entièeres et décim
+    #Séârer les parties entièeres et décimal
 	return ""
 
 def get_triangle(num_rows):
