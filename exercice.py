f7a87753245ddc595b2b251a33c8b04fa956dd19
matben001 <benoitm007@gmail.com>
2020-09-21 17:28:31 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -29,6 +29,7 @@ def format_number(number, num_decimal_digits):
     decimal_part = abs(number) % 1
     whole_part = int(number)
     #Formater la parties décimal
+    decimal_part = 
     #Formater la partie entières
     #Concaténer les deux parties
 	return ""
