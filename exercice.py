febbd9dbb6fb5c8988c7e8d965c882f5ae093da5
matben001 <benoitm007@gmail.com>
2020-09-21 17:28:07 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -27,6 +27,7 @@ def get_bill(name, data):
 def format_number(number, num_decimal_digits):
     #Séparer les parties entières et décimal
     decimal_part = abs(number) % 1
+    whole
     #Formater la parties décimal
     #Formater la partie entières
     #Concaténer les deux parties
